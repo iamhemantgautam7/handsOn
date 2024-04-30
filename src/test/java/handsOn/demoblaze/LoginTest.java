a5c87d48da29acfeb4eb265c48d88b586895978c
iamhemantgautam7 <iamhemantgautam7@gmail.com>
2024-04-30 13:19:52 +0000

    update

--- src/test/java/handsOn/demoblaze/LoginTest.java
+++ src/test/java/handsOn/demoblaze/LoginTest.java
@@ -1,17 +1,50 @@
 package handsOn.demoblaze;
 
+import org.testng.annotations.Test;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class LoginTest {
-	
+
 	WebDriver driver;
-	
-	
+	Properties prop;
+
 	@BeforeTest
-	public void initiatBrowser() {
-		driver=new ChromeDriver() ;
+	public void initiateSetup() throws IOException {
+
+		prop = new Properties();
+		String path = System.getProperty("user.dir") + "//src//test//resources//userCred.properties";
+		FileInputStream fis = new FileInputStream(path);
+		prop.load(fis);
+		fis.close();
+	}
+
+	@BeforeMethod
+	public void setup() {
+
+		String strBrowser = prop.getProperty("browser");
+		if (strBrowser.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		} else if (strBrowser.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		}
+
+	}
+
+	@Test
+	public void first_testCase() {
+
+		driver.get( prop.getProperty("url"));
+
 	}
 
 }
